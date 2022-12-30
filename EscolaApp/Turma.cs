78dd11c1a2b3e67b8e0efe90f22710cf3fcd01c8
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-12-30 17:34:36 -0300

    Update Turma.cs

--- EscolaApp/Turma.cs
+++ EscolaApp/Turma.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EscolaApp
 {
-    class Turma
+    public class Turma
     {
         public int id { get; set; }
         public string Curso { get; set; }
